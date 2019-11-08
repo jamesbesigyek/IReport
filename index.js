7227bb84745e131fe94bf2a716359a5b4c00448f
besigye <jamesbesigyek@gmail.com>
2019-11-08 17:13:33 +0300

    establishing server connections of html pages

---
+++ index.js
@@ -0,0 +1,74 @@
+/*establishing initial app parameters with express and body parser
+this is a nodeJS app with Mongo DB
+*/
+
+const express = require("express");
+const app = express();
+const path = require("path"); 
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({extended: true}))
+app.set("view engine", "pug")
+app.set("views", path.join(__dirname, "views")) //setting the views path
+
+
+    
+
+/* created an app of express object, then  listening on port.
+listen method takes two parameters, port number and function 
+listening is for development only..*/
+app.listen(5000,()=>{                           //arrow function
+    console.log("listening on port 5000")
+   
+
+})
+
+/*method for the landing page*/
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});      
+    
+
+
+
+
+
+app.get("/register",(req, res, next)=>{
+   // console.log("body", req.body)
+    //console.log("query params", req.query);
+    res.render("register")
+})
+// posting the form
+app.post("/register",(req, res, next)=>{
+    console.log("form has been posted")
+    console.log("body", req.body)
+    console.log("query params", req.query);
+    
+    res.render("page2")
+})
+
+
+
+
+
+//to run the Pug- local host:5000/pugfilename
+
+// sending data to another page
+
+app.post("/page2", (req, res)=>{    //'page2"path for page to be loaded-also similar in the action of loading page
+  //  console.log("hello world")
+    res.render("page2",             //"page2" is a name of the page to be shown
+    {  firstname:req.body.firstname,
+       lastname:req.body.lastname, 
+       email:req.body.email,
+       sex:req.body.gender,
+       city:req.body.city,
+       country:req.body.country,
+       password:req.body.password, //call fields by name
+
+       
+
+
+    }) //kevin is another pug file
+    console.log("body", req.body)
+})
